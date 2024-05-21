344c6c24aefeba417923c3b5e2add8e1265da962
Minamototennsai <Minamototennsai@gmail.com>
2024-05-21 15:03:16 +0800

    v0.1

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <HousewareTable />
+  </div>
+</template>
+
+<script>
+import HousewareTable from './components/HousewareTable.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HousewareTable
+  }
+};
+</script>
+
+<style>
+/* Add your global CSS styles here */
+</style>
